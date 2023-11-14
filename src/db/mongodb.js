beac41d6284b7e0f9501cffa4bfc8b0a93234238
Muhammad Hanan <hanan.ppqita@gmail.com>
2023-11-14 13:55:09 +0700

    commit

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
 export const connectionDB = async () => {
-    try {
-      await mongoose.connect('mongodb+srv://ppqita:santri@ppqitadb.dada60q.mongodb.net/karyawan', {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://ppqita:santri@ppqitadb.4jtlspc.mongodb.net/portal-karyawan",
+      {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-      }) 
-    } catch (error) {
-        console.log(error);
-    }
-  
-  };
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
